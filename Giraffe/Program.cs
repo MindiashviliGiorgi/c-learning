26af0c369a1e808d7649782ba6ad71329211faa5
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2023-11-23 03:42:12 +0400

    Numbers and User Input

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -43,6 +43,7 @@ namespace Giraffe
             */
 
             // 4 Working with Strings
+            /*
             string phrase = "Giraffe Academy";
             Console.WriteLine(phrase.Length); // sting length
             Console.WriteLine(phrase.ToUpper()); // upperCase
@@ -54,7 +55,31 @@ namespace Giraffe
             Console.WriteLine(phrase.IndexOf('G')); // return same but if we need to find single G we need '';
             Console.WriteLine(phrase.Substring(8, 3)); // if we give property only 8 its returning whole string after index 8
             // another way if we give Substring two property 8 will be start index and 3 will be that str.length
+            */
 
+            // 5 Working with Numbers
+            /*
+            Console.WriteLine( 11 % 3 );
+            int num = 6;
+            num++; // return 7 
+            num--; // return 5
+            Console.WriteLine( Math.Abs(-6) ); // return 6
+            Console.WriteLine(Math.Pow(3, 3)); // return 27
+            Console.WriteLine(Math.Sqrt(36)); // return 6
+            Console.WriteLine(Math.Max(19, 23)); // can take only two number and return bigger 23
+            Console.WriteLine(Math.Min(19, 23)); // can take only two number and return smaller 19
+            Console.WriteLine(Math.Round(3.4)); // return 3
+            Console.WriteLine(Math.Round(3.5)); // return 4
+            */
+
+            // 6 Getting User Input
+            /*
+            Console.Write("Enter your name : ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age : ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " Your age is " + age);
+            */
 
             Console.ReadLine();
         }
